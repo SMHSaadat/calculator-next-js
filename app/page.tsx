0ddf1323c5f1a77dc3a7e2ac5768202434eeff1a
SMHSaadat <hasanmicrob@gmail.com>
2024-06-13 15:08:59 +0330

    buttons properly

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,45 @@
 'use client'
 
 export default function Home() {
+
+
+const Btn = (props:any)=>{
+  return <button style={{height:60, width:60}} onClick={()=>{
+    (document.getElementById("txt") as HTMLInputElement).value = props.txt
+
+  }} > {props.txt}</button>
+}
+
   return (
     <div>
-        <button style={{height:60, width:60}}>-</button>
-        <button style={{height:60, width:60}}>1</button>
-        <button style={{height:60, width:60}}>2</button>
+      <input id="txt" style={{height:40, width:240}}></input>
+      <br/>
+        <Btn txt="-" />
+        <Btn txt="1" />
+        <Btn txt="2" />
+        <Btn txt="3" />
+        <br/>
+        <Btn txt="+" />
+        <Btn txt="4" />
+        <Btn txt="5" />
+        <Btn txt="6" />
+        <br/>
+        <Btn txt="0" />
+        <Btn txt="7" />
+        <Btn txt="8" />
+        <Btn txt="9" />
+        <br/>
+        <button style={{height:60, width:240}}>=</button>
+
+
+
+        <br/>
+
+
+
+
+
+        {/* <button style={{height:60, width:60}}>2</button>
         <button style={{height:60, width:60}}>3</button>
         <br/>
         <button style={{height:60, width:60}}>+</button>
@@ -18,7 +52,7 @@ export default function Home() {
         <button style={{height:60, width:60}}>8</button>
         <button style={{height:60, width:60}}>9</button>
         <br/>
-        <button style={{height:60, width:240}}>0</button>
+        <button style={{height:60, width:240}}>0</button> */}
     </div>
   );
 }
