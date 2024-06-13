060e3180c42642b851ad5e38fa03060bfab778f6
SMHSaadat <hasanmicrob@gmail.com>
2024-06-13 14:21:09 +0330

    add buttons

--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,23 @@
 
 export default function Home() {
   return (
-    <div align="center">
-      <h1>hello_world</h1>
-      <button onClick={
-        () => {
-          let s: any = prompt("Enter something")
-
-          alert("Your answer is: " + eval(s))
-        }}
-        style={{ padding: "20px 100px" }}>hii</button>
+    <div>
+        <button style={{height:60, width:60}}>-</button>
+        <button style={{height:60, width:60}}>1</button>
+        <button style={{height:60, width:60}}>2</button>
+        <button style={{height:60, width:60}}>3</button>
+        <br/>
+        <button style={{height:60, width:60}}>+</button>
+        <button style={{height:60, width:60}}>4</button>
+        <button style={{height:60, width:60}}>5</button>
+        <button style={{height:60, width:60}}>6</button>
+        <br/>
+        <button style={{height:60, width:60}}>0</button>
+        <button style={{height:60, width:60}}>7</button>
+        <button style={{height:60, width:60}}>8</button>
+        <button style={{height:60, width:60}}>9</button>
+        <br/>
+        <button style={{height:60, width:240}}>0</button>
     </div>
   );
 }
