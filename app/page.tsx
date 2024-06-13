bc7be51eedea10b93e279eaa47e65e8295205880
SMHSaadat <hasanmicrob@gmail.com>
2024-06-13 15:29:45 +0330

    implement calculator functionality

--- app/page.tsx
+++ app/page.tsx
@@ -1,58 +1,59 @@
 'use client'
 
+import { HtmlProps } from "next/dist/shared/lib/html-context.shared-runtime";
+
 export default function Home() {
 
 
-const Btn = (props:any)=>{
-  return <button style={{height:60, width:60}} onClick={()=>{
-    (document.getElementById("txt") as HTMLInputElement).value = props.txt
+  const Btn = (props: any) => {
+    return <button style={{ height: 60, width: 60 }} onClick={() => {
+      
+      let op = props.txt;
+      let el = (document.getElementById("txt") as HTMLInputElement);
 
-  }} > {props.txt}</button>
-}
+      if (op == "clear") {
+        el.value = "";
+        return;
+      }
+      if (op == "=") {
+        el.value = eval(el.value)
+      }
+      else {
+        el.value = el.value + op;
+      }
+
+    }} > {props.txt}</button>
+  }
+
+
+
 
   return (
     <div>
-      <input id="txt" style={{height:40, width:240}}></input>
-      <br/>
-        <Btn txt="-" />
-        <Btn txt="1" />
-        <Btn txt="2" />
-        <Btn txt="3" />
-        <br/>
-        <Btn txt="+" />
-        <Btn txt="4" />
-        <Btn txt="5" />
-        <Btn txt="6" />
-        <br/>
-        <Btn txt="0" />
-        <Btn txt="7" />
-        <Btn txt="8" />
-        <Btn txt="9" />
-        <br/>
-        <button style={{height:60, width:240}}>=</button>
+      <input id="txt" style={{ height: 40, width: 240 }}></input>
+      <br />
+      <Btn txt="-" />
+      <Btn txt="1" />
+      <Btn txt="2" />
+      <Btn txt="3" />
+      <br />
+      <Btn txt="+" />
+      <Btn txt="4" />
+      <Btn txt="5" />
+      <Btn txt="6" />
+      <br />
+      <Btn txt="0" />
+      <Btn txt="7" />
+      <Btn txt="8" />
+      <Btn txt="9" />
+      <br />
+      <Btn txt="="/>
+      <Btn txt="clear"/>
 
 
 
-        <br/>
+      <br />
 
-
-
-
-
-        {/* <button style={{height:60, width:60}}>2</button>
-        <button style={{height:60, width:60}}>3</button>
-        <br/>
-        <button style={{height:60, width:60}}>+</button>
-        <button style={{height:60, width:60}}>4</button>
-        <button style={{height:60, width:60}}>5</button>
-        <button style={{height:60, width:60}}>6</button>
-        <br/>
-        <button style={{height:60, width:60}}>0</button>
-        <button style={{height:60, width:60}}>7</button>
-        <button style={{height:60, width:60}}>8</button>
-        <button style={{height:60, width:60}}>9</button>
-        <br/>
-        <button style={{height:60, width:240}}>0</button> */}
     </div>
   );
 }
